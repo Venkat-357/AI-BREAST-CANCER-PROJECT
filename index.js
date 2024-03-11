15bd7424f6af3c0b153fc9c8001ce357a9163d1b
venkat <bhaskarv357@gmail.com>
2024-03-11 13:44:46 +0530

    first push

---
+++ index.js
@@ -0,0 +1,7 @@
+$(".btn-outline-secondary").click(function(){
+    prompt("which type of image you want to upload");
+});
+
+$(".btn-primary").click(function(){
+    alert("generating the predictions");
+})
